f31d56bd4e2accba9f8b1ffa78c63b54640aabd4
vanita22 <vanitayrafita2004@gmail.com>
2020-11-26 17:54:13 -0400

    cargando componente modal

--- src/components/card.js
+++ src/components/card.js
@@ -7,7 +7,7 @@ function Card(props){
     return (
         <div className="card-container">
             <p>{props.id} {props.name}</p>
-            <img src={props.img} alt={props.name} onClick={() => setShow(!show)}/>
+            <img src={props.img} alt={props.name} onClick={() => setShow(!show)}/> {/*nuevo agregado para abrir y cerrar el modal selecionando las imagenes de los pokemones*/}
             {                    
                 show ? <Modal  
                             name={props.name} 
