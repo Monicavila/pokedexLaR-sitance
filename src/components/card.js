77f2c2c85bdbd3df9e4e86aa50d69fd56f6f219f
vanita22 <vanitayrafita2004@gmail.com>
2020-11-26 16:21:44 -0400

    subiendo componente modal

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState} from 'react';
+import Modal from './modal/modal.jsx';
 
 function Card(props){
+    const [show, setShow] = useState(false);
+
     return (
         <div className="card-container">
             <p>{props.id} {props.name}</p>
-            <img src={props.img} alt={props.name} />
+            <img src={props.img} alt={props.name} onClick={() => setShow(!show)}/>
+            {                    
+                show ? <Modal  
+                            name={props.name} 
+                            img={props.img}
+                            id={props.id}
+                            setShow={setShow} /> : null                      
+            }
         </div>
     )
 }
